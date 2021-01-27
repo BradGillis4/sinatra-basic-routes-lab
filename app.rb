4ab6e950c215b9d20d11e0d101a50fdf5d02fb17
BradGillis4 <bradgillis4@gmail.com>
2021-01-27 14:05:19 -0500

    commit tree 8e9c22cb1903709d100ecbc7830395636e7422b0

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-end
+
+    class App < Sinatra::Base
+        get('/name') {
+          "My name is Brad"
+        }
+        get('/hometown') {
+          "My hometown is Warren"
+        }
+        get('/favorite-song') {
+          "My favorite song is Colors"
+        }
+      end
